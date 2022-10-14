3843aa0c7b762cf39ada6980166c80dfa897efcc
FazliddinAsadov <asadovfazliddin11@gmail.com>
2022-10-14 11:24:30 +0500

    create new folders

---
+++ src/modules/dashboard/modules/admins/Admins.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type Props = {};
+
+const Admins = (props: Props) => {
+  return <div>Admins</div>;
+};
+export default Admins;
---
+++ src/modules/dashboard/modules/users/Users.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type Props = {};
+
+const Users = (props: Props) => {
+  return <div>Users</div>;
+};
+export default Users;
