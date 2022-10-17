a109ee153d081b81e05aa04e6e2ab073b69ca40a
Isroiljon <110961908+Isroiljon5355@users.noreply.github.com>
2022-10-17 16:10:17 +0500

    Ft #3 (#5)
    * table
    
    * tableda delet qo'shdim
    
    * langujni torladim
    
    * delets

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,36 +1,122 @@
-import { Table } from "@mantine/core";
-import React from "react";
-
-const elements = [
-  { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
-  { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
-  { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
-  { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
-  { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
-];
+import useUser from "@hooks/shared/useUser";
+import { Table, Text } from "@mantine/core";
+import { openConfirmModal } from "@mantine/modals";
+import { showNotification, updateNotification } from "@mantine/notifications";
+import adminFetchers from "@services/api/adminFetchers";
+import { IconCheck, IconTrash } from "@tabler/icons";
+import { RequestQueryKeys } from "@utils/constants";
+import useSWR, { mutate, useSWRConfig } from "swr";
 
 function TableCard() {
-  const rows = elements.map((element) => (
-    <tr key={element.name}>
-      <td>{element.position}</td>
-      <td>{element.name}</td>
-      <td>{element.symbol}</td>
-      <td>{element.mass}</td>
-    </tr>
-  ));
+  const { mutate: deleteAdmin } = useSWRConfig();
+  const { name, _id } = useUser();
+  console.log(name);
+  console.log(name);
+
+  const {
+    data,
+    error,
+    mutate: refetch,
+  } = useSWR(RequestQueryKeys.getAdmins, adminFetchers.getAdmins);
+
+  if (error) return <div>yuklash xatosi</div>;
+  if (!data) return <div>yuklanmoqda...</div>;
+
+  const handleDelete = async function (id: string) {
+    const res = await deleteAdmin(
+      RequestQueryKeys.deleteAdmin,
+      adminFetchers.deleteAdmin(id),
+      {
+        revalidate: true,
+      }
+    );
+    updateNotification({
+      id: "load-data",
+      color: "teal",
+      title: "Data was loaded",
+      message:
+        "Notification will close in 2 seconds, you can close this notification now",
+      icon: <IconCheck size={16} />,
+      autoClose: 2000,
+    });
+    refetch();
+    console.log(res);
+  };
+
+  const openDeleteModal = (id: string, name: string) =>
+    openConfirmModal({
+      title: "Delete your profile",
+      centered: true,
+      children: (
+        <Text size="sm">
+          {name} Ushbu ishchingiz sizni tizimdan o&apos;chib ketadi, bu ishni
+          ortga qaytarib bo&apos;lmaydi, shunda ham ishonchingiz komilmi?
+        </Text>
+      ),
+      labels: { confirm: "Tasdiqlash", cancel: "Bekor qilish" },
+      confirmProps: { color: "red" },
+      onConfirm: async () => {
+        showNotification({
+          id: "load-data",
+          loading: true,
+          title: "Loading your data",
+          message: "dsklmqpoa",
+          autoClose: false,
+          disallowClose: true,
+        });
+        handleDelete(id);
+      },
+
+      onCancel: () => {
+        showNotification({
+          title: "Siz bekor qildingiz",
+          message: "Hey there, your code is awesome! 🤥",
+        });
+      },
+    });
+
+  const rows = data.map((item: any) => {
+    console.log(item._id);
+
+    return (
+      <tr key={item._id}>
+        <td>{item.name}</td>
+        <td>{item.email}</td>
+        <td>{item.phone}</td>
+        <td>
+          {/* <IconTrash
+            onClick={() => openDeleteModal(item._id, item.name)}
+            // onClick={() => handleDelete(item._id)}
+            // onClick={() => openDeleteModal(item._id)}
+            style={{ color: "red", cursor: "pointer" }}
+          /> */}
+          {item.name == name ? (
+            <IconTrash style={{ color: "red", cursor: "no-drop" }} />
+          ) : (
+            <IconTrash
+              onClick={() => openDeleteModal(item._id, item.name)}
+              style={{ color: "red", cursor: "pointer" }}
+            />
+          )}
+        </td>
+      </tr>
+    );
+  });
 
   return (
-    <Table striped>
-      <thead>
-        <tr>
-          <th>Element position</th>
-          <th>Element name</th>
-          <th>Symbol</th>
-          <th>Atomic mass</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </Table>
+    <>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Ismi</th>
+            <th>Elektron po&lsquo;chta</th>
+            <th>Telefon nomeri</th>
+            <th>o&lsquo;chirish va tahrirlash</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </>
   );
 }
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,4 +6,5 @@ export const appRoutes = {
 export enum RequestQueryKeys {
   getAdmins = "admins",
   login = "login",
+  deleteAdmin = "deleteAdmin",
 }
