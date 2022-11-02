6515f51855e3d4d73163c634f96747297fade737
jakhongir-abdukhamidov <jakhongirabdukhamidov@gmail.com>
2022-11-02 21:52:54 +0500

    ilovaga aylantirildi

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+});
+
+const nextConfig = withPWA({
   reactStrictMode: false,
   swcMinify: true,
-};
+});
 
 module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ import { ModalsProvider } from "@mantine/modals";
 import { NotificationsProvider } from "@mantine/notifications";
 import flattenMessages from "@utils/flattenMessages";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Script from "next/script";
 import { ErrorBoundary } from "react-error-boundary";
 import { IntlProvider } from "react-intl";
@@ -36,6 +37,50 @@ function MyApp({ Component, pageProps, router }: AppProps) {
 
   return (
     <IntlProvider messages={flattenMessages(uz)} locale="uz" defaultLocale="uz">
+      <Head>
+        <title>Tokar savdo</title>
+        <meta name="application-name" content="Store system" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Tokar store" />
+        <meta
+          name="description"
+          content="Developed by Tespen Software Services"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="author" content="Jakhongir Abdukhamidov" />
+        <meta name="copyright" content="Jakhongir Abdukhamidov" />
+
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json"></link>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <AppWrapper>
         <ErrorBoundary FallbackComponent={ErrorFallback} onReset={forceUpdate}>
           <ProtectedRoute router={router}>
