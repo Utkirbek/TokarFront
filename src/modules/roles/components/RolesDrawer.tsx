1862e8b02bce8c7740f443c0ce19d6a26a00bc5d
Utkirbek <utkirbek.me@gmail.com>
2022-11-04 16:54:05 +0500

    roles finished

--- src/modules/roles/components/RolesDrawer.tsx
+++ src/modules/roles/components/RolesDrawer.tsx
@@ -1,81 +1,97 @@
 import {
-    Box,
-    Button,
-    Group,
-    MultiSelect,
-    Text,
-    TextInput,
-  } from "@mantine/core";
-  import { useForm } from "@mantine/form";
-  import { showNotification, updateNotification } from "@mantine/notifications";
-  import useRoles from "@services/hooks/useRoles";
-  import { IconCheck, IconLock } from "@tabler/icons";
-  
-  const RolesDrawer: React.FC<{
-    handleClose: () => void;
-    editItem: any;
-  }> = ({ handleClose, editItem }) => {
-    const { editRole, addRole } = useRoles();
-  
-    const form = useForm({
-      initialValues: {
-        name: editItem?.name ?? "",
-        permissions: editItem?.permissions ?? [],
-      },
-      validate: {
-        name: (value: string | any[]) =>
-          value.length < 2 ? "Rol nomi 2ta belgidan ko'p bo'lishi kerak" : null
+  Box,
+  Button,
+  Group,
+  MultiSelect,
+  Text,
+  TextInput,
+} from "@mantine/core";
+import WithLoading from "@/hoc/WithLoading";
+import { useForm } from "@mantine/form";
+import { showNotification, updateNotification } from "@mantine/notifications";
+import useRoles from "@services/hooks/useRoles";
+import useSettings from "@services/hooks/useSettings";
+import { IconCheck } from "@tabler/icons";
+
+
+
+const RolesDrawer: React.FC<{
+  handleClose: () => void;
+  editItem: any;
+}> = ({ handleClose, editItem }) => {
+  console.log(editItem);
+  const { editRole, addRole } = useRoles();
+  const { useFetchAllPermissions } = useSettings();
+  const getRolesQuery = useFetchAllPermissions();
+  const { data: permissions } = getRolesQuery;
+
+  const DefaultPermissions = editItem?.permissions?.map(
+    (permission: any) => permission._id
+  );
+  const Permissions = permissions?.map((permission: any) => ({
+    label: permission.name,
+    value: permission._id,
+  }));
+  const form = useForm({
+    initialValues: {
+      name: editItem?.name ?? "",
+      permissions: editItem?.permissions ?? [],
+    },
+    validate: {
+      name: (value: string | any[]) =>
+        value.length < 2 ? "Rol nomi 2ta belgidan ko'p bo'lishi kerak" : null,
+    },
+  });
+
+  const handleSubmit = async (values: {
+    name: string;
+    permissions: string[];
+  }) => {
+    if (!!editItem._id) {
+      handleClose();
+      editRole(
+        {
+          id: editItem._id,
+          values,
+        },
+        {
+          onSuccess: () => {
+            updateNotification({
+              id: "load-data",
+              color: "teal",
+              title: "Muaffaqiyatli",
+              message: "Sizning mahsuloringiz Yangilandi",
+              icon: <IconCheck size={16} />,
+              autoClose: 2000,
+            });
+          },
+        }
+      );
+    }
+    showNotification({
+      id: "load-data",
+      loading: true,
+      title: "Iltimos kuting",
+      message: "Sizning mahsuloringiz qo'shilmoqda iltimos kuting",
+      autoClose: false,
+      disallowClose: true,
+    });
+
+    addRole(values, {
+      onSuccess: () => {
+        updateNotification({
+          id: "load-data",
+          color: "teal",
+          title: "Muaffaqiyatli",
+          message: "Sizning mahsuloringiz Qo'shildi",
+          icon: <IconCheck size={16} />,
+          autoClose: 2000,
+        });
       },
     });
-  
-    const handleSubmit = async (values: {
-      name: string;
-        permissions: string[];  
-    }) => {
-      if (!!editItem._id) {
-        handleClose();
-        editRole(
-          {
-            id: editItem._id,
-            values,
-          },
-          {
-            onSuccess: () => {
-              updateNotification({
-                id: "load-data",
-                color: "teal",
-                title: "Muaffaqiyatli",
-                message: "Sizning mahsuloringiz Yangilandi",
-                icon: <IconCheck size={16} />,
-                autoClose: 2000,
-              });
-            },
-          }
-        );
-      }
-      showNotification({
-        id: "load-data",
-        loading: true,
-        title: "Iltimos kuting",
-        message: "Sizning mahsuloringiz qo'shilmoqda iltimos kuting",
-        autoClose: false,
-        disallowClose: true,
-      });
-  
-      addRole(values, {
-        onSuccess: () => {
-          updateNotification({
-            id: "load-data",
-            color: "teal",
-            title: "Muaffaqiyatli",
-            message: "Sizning mahsuloringiz Qo'shildi",
-            icon: <IconCheck size={16} />,
-            autoClose: 2000,
-          });
-        },
-      });
-    };
-    return (
+  };
+  return (
+    <WithLoading query={getRolesQuery}>
       <Box mx="auto">
         <form onSubmit={form.onSubmit(handleSubmit)}>
           <Text size={"xl"} weight={700}>
@@ -90,6 +106,13 @@ import {
             my={"sm"}
             required
           />
+          <MultiSelect
+            defaultValue={DefaultPermissions ?? []}
+            name="permissions"
+            data={Permissions}
+            label="Rol uchun ruxsatlarni tanlang"
+            placeholder="Ruxsatlarni tanlang"
+          />
           <Group position="right" mt="md">
             <Button type="submit">
               {!editItem._id ? "Ro'yxatga Qo'shish" : "Saqlash"}
@@ -97,7 +120,8 @@ import {
           </Group>
         </form>
       </Box>
-    );
-  };
-  export default RolesDrawer;
+    </WithLoading>
+  );
+};
+export default RolesDrawer;
   
