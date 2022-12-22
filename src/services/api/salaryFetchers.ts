2c81d0d0ab0198f0895735d54eb41b16e4044474
Utkirbek <dovutkhanmutaliyev@gmail.com>
2022-12-22 21:02:57 +0500

    some changes

--- src/services/api/salaryFetchers.ts
+++ src/services/api/salaryFetchers.ts
@@ -1,8 +1,15 @@
+import { getCookie } from "cookies-next";
+
 import requests from "./requests";
 
 const salaryFetchers = {
-  getSalary: async (id: string) => requests.get("/admin/salary"),
-  addSalary: async <T>(body: T) => await requests.post("/admin/salary", body),
+  
+  getSalary: async (id: string) => {
+    const shopId = getCookie("shopId");
+    return requests.get("/admin/salary")},
+  addSalary: async <T>(body: T) => {
+    const shopId = getCookie("shopId");
+    return await requests.post("/admin/salary", body)},
 };
 
 export default salaryFetchers;
